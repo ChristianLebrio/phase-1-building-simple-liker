16aa5e346e0e1cbc32835a7d9c7a6ef8740ba47a
Christian Lebrio <ChristianLebrio@gmail.com>
2023-02-05 01:05:31 -0500

    solved tests

--- main.js
+++ main.js
@@ -4,6 +4,36 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+let likeButtons = document.getElementsByClassName("like");
+
+for (let i = 0; i < likeButtons.length; i++) {
+  likeButtons[i].addEventListener("click", heartFunction)
+}
+
+function heartFunction (e){
+  console.log(e)
+
+  mimicServerCall()
+  .then(() => {
+   if (e.target.innerText = EMPTY_HEART){
+    e.target.classList.add("activated-heart")
+    e.target.innerText = FULL_HEART
+   }
+   else {
+    e.target.classList.remove("activated-heart")
+    e.target.innerText = EMPTY_HEART
+   }
+
+  })
+  .catch((rejectMessage) => {
+    let errorModal = document.getElementById("modal");
+    errorModal.classList.remove("hidden");
+    errorModal.children[1].innerText = rejectMessage;
+    setTimeout(() => {errorModal.classList.add("hidden")}, 3000)
+  })
+}
+
+
 
 
 
